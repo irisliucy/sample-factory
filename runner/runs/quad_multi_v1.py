498016aebfc00ef480607673bcd13580ab629c8f
Zhehui Huang <zhehuihu@usc.edu>
2020-08-14 02:25:36 -0700

    Modify two run files

--- runner/runs/quad_multi_v1.py
+++ runner/runs/quad_multi_v1.py
@@ -2,20 +2,20 @@ from runner.run_description import RunDescription, Experiment, ParamGrid
 
 _params = ParamGrid([
     ('seed', [0000, 1111, 2222, 3333, 4444, 5555, 6666, 7777]),
+    ('batch_size', [512]),
     ('ppo_epochs', [1]),
-    ('learning_rate', [0.0001]),
+    ('nonlinearity', ['tanh']),
+    ('learning_rate', [1e-4]),
     ('entropy_loss_coeff', [0.0]),
+    ('initial_stddev', [1.0]),
+    ('hidden_size', [64]),
+    ('adam_eps', [1e-8]),
 ])
 
 _experiment = Experiment(
     'quads_gridsearch',
-    'python -m algorithms.appo.train_appo --env=quadrotor_multi --train_for_env_steps=1000000000 --algo=APPO '
-    '--gamma=0.99 --use_rnn=False --num_workers=72 --num_envs_per_worker=2 --num_policies=1 --rollout=128 '
-    '--recurrence=1 --batch_size=512 --benchmark=False  --with_pbt=False --adam_eps=1e-8 --nonlinearity=tanh '
-    '--actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False '
-    '--hidden_size=64 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=0.0 '
-    '--ppo_clip_value=5.0',
+    'python -m run_algorithm --env=quadrotor_multi --train_for_env_steps=1000000000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=72 --num_envs_per_worker=2 --num_policies=1 --rollout=128 --recurrence=1 --benchmark=False --with_pbt=False --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=0.0 --ppo_clip_value=5.0',
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('quads_multi_v104_8seeds', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('quads_single_v104_8seeds', experiments=[_experiment])
--- runner/runs/quad_single_v1.py
+++ runner/runs/quad_single_v1.py
@@ -1,20 +1,21 @@
 from runner.run_description import RunDescription, Experiment, ParamGrid
 
 _params = ParamGrid([
+    ('seed', [0000, 1111, 2222, 3333, 4444, 5555, 6666, 7777]),
+    ('batch_size', [512]),
     ('ppo_epochs', [1]),
-    ('learning_rate', [0.0001]),
+    ('nonlinearity', ['tanh']),
+    ('learning_rate', [1e-4]),
+    ('entropy_loss_coeff', [0.0]),
+    ('initial_stddev', [1.0]),
+    ('hidden_size', [64]),
+    ('adam_eps', [1e-8]),
 ])
 
 _experiment = Experiment(
-    'quads_pbt',
-    'python -m algorithms.appo.train_appo --env=quadrotor_single --train_for_env_steps=1000000000 --algo=APPO '
-    '--gamma=0.99 --use_rnn=False --num_workers=72 --num_envs_per_worker=4 --num_policies=8 --rollout=128 '
-    '--recurrence=1 --batch_size=512 --benchmark=False --pbt_replace_reward_gap=0.1 --pbt_period_env_steps=1000000 '
-    '--pbt_start_mutation=20000000 --with_pbt=True --adam_eps=1e-8 --nonlinearity=tanh '
-    '--actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False '
-    '--hidden_size=64 --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=0.0 '
-    '--ppo_clip_value=5.0',
+    'quads_gridsearch',
+    'python -m run_algorithm --env=quadrotor_single --train_for_env_steps=1000000000 --algo=APPO --gamma=0.99 --use_rnn=False --num_workers=72 --num_envs_per_worker=8 --num_policies=1 --rollout=128 --recurrence=1 --benchmark=False --with_pbt=False --actor_critic_share_weights=False --policy_initialization=xavier_uniform --adaptive_stddev=False --with_vtrace=False --max_policy_lag=100000000 --gae_lambda=1.00 --max_grad_norm=0.0 --ppo_clip_value=5.0',
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription('quads_single_pbt_v102_v3', experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription('quads_single_v104_8seeds', experiments=[_experiment])
